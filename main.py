04e6f19c83c50b069c0f2ef3328d900b7cf60b84
Yan <nezd@protonmail.com>
2020-03-07 18:06:26 +0300

    Add ability to change plugin folder

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from pyrogram import Client, MessageHandler
 import settings
 
 
-app = Client("my_account", api_id=settings.API_ID, api_hash=settings.API_HASH, proxy=settings.PROXY, plugins={"root":"plugins"})
+app = Client("my_account", api_id=settings.API_ID, api_hash=settings.API_HASH, proxy=settings.PROXY, plugins={"root":settings.PLUGIN_FOLDER})
 
 if __name__ == '__main__':
     app.run()
